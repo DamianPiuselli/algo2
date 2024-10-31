53e69b8c0ba129fe858ef1422d994094fcc5789a
DamianPiuselli <damianpiuselli@gmail.com>
2024-10-31 14:43:53 -0300

    dfs y bfs en grafo conjuntos

--- 04_grafos/ej_grafos_con_conjuntos.py
+++ 04_grafos/ej_grafos_con_conjuntos.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Generic, Set, Tuple, List
+from typing import TypeVar, Generic, Set, Tuple, List, Optional
 
 T = TypeVar("T")
 
@@ -7,10 +7,17 @@ T = TypeVar("T")
 
 class Grafo(Generic[T]):
     def __init__(
-        self, vertices: Set[T] = set(), aristas: Set[Tuple[T, T]] = set()
+        self,
+        vertices: Optional[Set[T]] = None,
+        aristas: Optional[Set[Tuple[T, T]]] = None,
     ) -> None:
-        self.vertices = vertices  # Conjunto de vértices o nodos del grafo
-        self.aristas = aristas  # Conjunto de aristas, que son pares de vértices
+        self.vertices = (
+            vertices if vertices else set()
+        )  # Conjunto de vértices o nodos del grafo
+
+        self.aristas = (
+            aristas if aristas else set()
+        )  # Conjunto de aristas, que son pares de vértices
 
     def agregar_nodo(self, nodo: T) -> None:
         if nodo in self.vertices:
@@ -49,6 +56,54 @@ class Grafo(Generic[T]):
     def __str__(self) -> str:
         return f"\nVertices: {self.vertices}\nAristas: {self.aristas}\n"
 
+    def dfs(self) -> List[T]:
+        visitados = set()  # almaceno los nodos visitados
+        recorrido = []
+
+        def dfs_recursivo(nodo):
+            visitados.add(nodo)
+            recorrido.append(nodo)
+
+            # Recorrer los vecinos del nodo actual recursivamente con recursion de cola
+            # la condicion al recorrer vecinos es que estos no hayan sido visitados
+            for vecino in self.vecinos_de(nodo):
+                if vecino not in visitados:
+                    dfs_recursivo(vecino)
+
+            # si el grafo no es conexo.
+            if len(visitados) < len(self.vertices):
+                for vertice in self.vertices:
+                    if vertice not in visitados:
+                        dfs_recursivo(vertice)
+
+        if len(self.vertices) > 0:
+            dfs_recursivo(next(iter(self.vertices)))
+
+        return recorrido
+
+    def bfs(self) -> List[T]:
+        visitados = set()
+        recorrido = []
+
+        # cola para almacenar los nodos a visitar
+        cola = []
+
+        # agrego a la cola los nodos que no han sido visitados. Me permite extender bfs a grafos no conexos.
+        for vertice in self.vertices:
+            if vertice not in visitados:
+                cola.append(vertice)
+
+                while cola:
+                    nodo = cola.pop(0)
+                    visitados.add(nodo)
+                    recorrido.append(nodo)
+
+                    for vecino in self.vecinos_de(nodo):
+                        if vecino not in visitados:
+                            cola.append(vecino)
+
+        return recorrido
+
 
 def main():
     grafo = Grafo()
@@ -74,6 +129,28 @@ def main():
     # Test vecinos_de
     print("Neighbors of 'A':", grafo.vecinos_de("A"))
     print("Neighbors of 'B':", grafo.vecinos_de("B"))
+
+    # test DFS en grafo no dirigido y conexo.
+    grafo2 = Grafo()
+
+    # Add nodes
+    grafo2.agregar_nodo("A")
+    grafo2.agregar_nodo("B")
+    grafo2.agregar_nodo("C")
+    grafo2.agregar_nodo("D")
+    grafo2.agregar_nodo("E")
+    grafo2.agregar_nodo("F")
+
+    grafo2.agregar_arista("A", "B")
+    grafo2.agregar_arista("A", "C")
+    grafo2.agregar_arista("B", "D")
+    grafo2.agregar_arista("B", "E")
+    grafo2.agregar_arista("C", "F")
+
+    print(grafo2)
+
+    print(f"recorrido DFS: {grafo2.dfs()}")
+    print(f"recorrido BFS: {grafo2.bfs()}")
 
 
 if __name__ == "__main__":
